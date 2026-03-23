5c68765a395c18e244c285f292485f72b0de61a0
alanbritodelgado123 <alanbrito4280@gmail.com>
2026-03-23 16:17:36 -0400

    si

--- src/input.rs
+++ src/input.rs
@@ -21,9 +21,10 @@ pub fn leer_teclado() -> Vec<Accion> {
     if is_key_pressed(KeyCode::Left)   { acciones.push(Accion::Izquierda); }
     if is_key_pressed(KeyCode::Right)  { acciones.push(Accion::Derecha); }
     if is_key_pressed(KeyCode::Z)      { acciones.push(Accion::BotonA); }
+    if is_key_pressed(KeyCode::Enter)  { acciones.push(Accion::BotonA); }
     if is_key_pressed(KeyCode::X)      { acciones.push(Accion::BotonB); }
+    if is_key_pressed(KeyCode::Escape) { acciones.push(Accion::BotonB); }
     if is_key_pressed(KeyCode::M)      { acciones.push(Accion::Mapa); }
-    if is_key_pressed(KeyCode::Escape) { acciones.push(Accion::Menu); }
     if is_key_pressed(KeyCode::L)      { acciones.push(Accion::Libreta); }
     acciones
 }
